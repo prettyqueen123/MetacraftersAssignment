1a027367cb47bfe8ba9aa59c77ac6890e881c291
prettyqueen123 <146948653+prettyqueen123@users.noreply.github.com>
2023-10-04 13:29:22 +0000

    Update prettycoin.sol

--- ethProofbeginner/prettycoin.sol
+++ ethProofbeginner/prettycoin.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
 
+contract PrettyCoin {
+    string public name = "PrettyCoin";
+    string public symbol = "PC";
+    uint256 public totalSupply;
+    address public owner;
+    mapping(address => uint256) public balances;
+
+    function mint(address receiver, uint256 value) public {
+        totalSupply += value;
+        balances[receiver] += value;
+    }
+
+    function burn(address receiver, uint256 value) public {
+        require(balances[receiver] >= value, "Insufficient balance to burn");
+        totalSupply -= value;
+        balances[receiver] -= value;
+    }
+}
